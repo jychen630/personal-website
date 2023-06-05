5f197d7684782936734dc620136253e9022e6537
AcKindle3 <jychen630@outlook.com>
2023-06-05 01:48:02 -0400

    staged

--- app.py
+++ app.py
@@ -29,37 +29,7 @@ def demo():
 def maxcut():
     return render_template('maxcut.html', common=common, slides=printHtml())
 
-def order_projects_by_weight(projects):
-    try:
-        return int(projects['weight'])
-    except KeyError:
-        return 0
 
-
-@app.route('/projects/<title>')
-def project(title):
-    projects = get_static_json("static/projects/projects.json")['projects']
-    experiences = get_static_json("static/experiences/experiences.json")['experiences']
-
-    in_project = next((p for p in projects if p['link'] == title), None)
-    in_exp = next((p for p in experiences if p['link'] == title), None)
-
-    if in_project is None and in_exp is None:
-        return render_template('404.html'), 404
-    # fixme: choose the experience one for now, cuz I've done some shite hardcoding here.
-    elif in_project is not None and in_exp is not None:
-        selected = in_exp
-    elif in_project is not None:
-        selected = in_project
-    else:
-        selected = in_exp
-
-    # load html if the json file doesn't contain a description
-    if 'description' not in selected:
-        path = "experiences" if in_exp is not None else "projects"
-        selected['description'] = io.open(get_static_file(
-            'static/%s/%s/%s.html' % (path, selected['link'], selected['link'])), "r", encoding="utf-8").read()
-    return render_template('project.html', common=common, project=selected)
 
 
 @app.errorhandler(404)
